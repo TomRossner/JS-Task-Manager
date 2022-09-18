0eb363cc7d14393499477b1f7dcc9c010b99eb65
Tom Rossner <tomrossner2@gmail.com>
2022-09-18 23:02:09 +0300

    Update

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const tasksContainer = document.getElementById("tasksContainer");
 const tasksList = TasksManager.getTasks();
 const errorContainer = document.getElementById("errors");
 const minimumCharacters = 3;
-const maximumCharacters = 35;
+const maximumCharacters = 25;
 const myTasksTitle = document.getElementById("myTasksTitle");
 const noTasks = document.getElementById("noTasks");
 const clearAllBtn = document.getElementById("clearTasks");
@@ -33,7 +33,6 @@ tasksContainer.addEventListener("click", function (e){
 function isValid(event){
     const input = document.getElementById("taskInput");
     if(event.key === "Enter" && input.value.length > minimumCharacters && input.value.length <= maximumCharacters){
-        console.log(input.value.length);
         createTask();
         errorContainer.style.opacity = 0;
         return;
