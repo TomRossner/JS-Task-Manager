5c0c456d63ec0ddb7c6368e7c900665acd0060ce
Tom Rossner <tomrossner2@gmail.com>
2022-10-03 08:51:53 +0300

    Update

--- script.js
+++ script.js
@@ -1,47 +1,23 @@
-const TasksManager = new TaskManager();
-renderTasks();
 const addBtn = document.getElementById("addButton");
-const checkButtons = document.querySelectorAll(".btn.check");
-const removeButtons = document.querySelectorAll(".remove");
-const editButtons = document.querySelectorAll(".btn.edit");
-const inputText = document.getElementById("taskInput").value;
-const tasksContainer = document.getElementById("tasksContainer");
-const tasksList = TasksManager.getTasks();
-const errorContainer = document.getElementById("errors");
 const minimumCharacters = 3;
 const maximumCharacters = 30;
-const myTasksTitle = document.getElementById("myTasksTitle");
+const errorContainer = document.getElementById("errors");
+const TasksManager = new TaskManager();
 const noTasks = document.getElementById("noTasks");
-const clearAllBtn = document.getElementById("clearTasks");
-clearAllBtn.addEventListener("click", clearAllTasks);
-enableClearAllBtn();
-loadEventListener();
-displayTitle();
-
-
-// Event Listeners
-addBtn.addEventListener("click", isValid);
-document.addEventListener("keydown", (e) => {isValid(e);})
-tasksContainer.addEventListener("click", function (e){
-    if(e.target.classList.contains("bi-trash3") || e.target.classList.contains("remove")){
-        removeTask(e);
-        return;
-    }
-    if(e.target.classList.contains("edit") || e.target.classList.contains("bi-pencil-fill")){
-        editTask(e);
-    }
-})
+const tasksList = TasksManager.getTasks();
+const myTasksTitle = document.getElementById("myTasksTitle");
 
 
 // Functions
-function isValid(event){
+
+const isValid = (event) => {
     const input = document.getElementById("taskInput");
     if(event.key === "Enter" && input.value.length >= minimumCharacters && input.value.length <= maximumCharacters){
         createTask();
         errorContainer.style.opacity = 0;
         return;
     }
-    if(event.key === "Enter" && input.value.length >= 1 || event.key === "Enter" && input.value === ""){
+    if(event.key === "Enter" && input.value.length >= 1){
         errorContainer.innerHTML = `Task must contain at least ${minimumCharacters} characters.`;
         errorContainer.style.opacity = 1;
     }
@@ -52,7 +28,6 @@ function isValid(event){
 
 
     if(event.type === "click" && input.value.length >= minimumCharacters && input.value.length <= maximumCharacters){
-        console.log(input.value.length);
         createTask();
         errorContainer.style.opacity = 0;
         return;
@@ -66,58 +41,26 @@ function isValid(event){
         errorContainer.style.opacity = 1;
     }
 }
-
-function removeTask(event){
-    const currentTask = (event.target.closest(".btn").closest(".task"));
-    const currentTaskID = Number(currentTask.getAttribute("data-id"));
-    TasksManager.deleteTask(currentTaskID)
-    TasksManager.save();
-    removeTaskFromDOM(currentTask);
-    displayTitle();
+const createTask = () => {
+    const tasksContainer = document.getElementById("tasksContainer")
+    tasksContainer.innerHTML = "";
+    const inputText = document.getElementById("taskInput").value;
+    TasksManager.add(inputText);
+    const myTasksTitle = document.createElement("h2");
+    myTasksTitle.className = "title";
+    myTasksTitle.innerHTML = `My Tasks`;
+    tasksList.length ? tasksContainer.append(myTasksTitle) : "";
+    renderTasks(tasksList);
+    clearInput();
+    loadEventListeners();   
     enableClearAllBtn();
 }
-
-function removeTaskFromDOM(element){
-    element.remove();
-}
-
-function checkIcons(task){
-    if(task.isCompleted){
-        return `<span><i class="bi bi-arrow-counterclockwise"></i></span>`;
-    }else{
-        return `<span><i class="bi bi-check2"></i></span>`;
-    }
-}
-
-function isComplete(task){
-    if(task.isCompleted){
-        return "task completed"
-    }
-    if(!task.isCompleted){
-        return "task";
-    }
-}
-
-function updateCompletionBtn(button){
-    const currentTask = (button.closest(".task"));
-    const currentTaskInput = currentTask.getElementsByTagName("input")[0];
-    if(!currentTask.classList.contains("completed")){
-        currentTask.classList.add("completed")
-        TasksManager.updateCompletion(TasksManager.getTasks().find(task => task.task === currentTaskInput.value), true)
-        return;
-    }else {
-        currentTask.classList.remove("completed");
-        TasksManager.updateCompletion(TasksManager.getTasks().find(task => task.task === currentTaskInput.value), false);
-        return;
-    }
-}
-
-function renderTasks(){
+const renderTasks = (list) => {
     const tasksContainer = document.getElementById("tasksContainer");
-    const tasksList = TasksManager.getTasks();
-    for(let task of tasksList){
-        let taskDiv = document.createElement("div");
-        taskDiv.className = `${isComplete(task)}`;
+    for(let task of list){
+        const taskDiv = document.createElement("div");
+        task.isCompleted ? taskDiv.className = "task completed" : taskDiv.className = "task";
+        taskDiv.innerHTML = `<button class="btn check"><span><i class="bi bi-check2"></i></span></button>`;
         taskDiv.setAttribute(`data-id`, `${TasksManager.getTasks().indexOf(task)}`);
         const newTaskInput = document.createElement("input");
         newTaskInput.defaultValue = `${task.task}`;
@@ -125,91 +68,56 @@ function renderTasks(){
         newTaskInput.setAttribute("type", "text");
         newTaskInput.setAttribute("readOnly", "enabled");
         taskDiv.append(newTaskInput);
-        taskDiv.innerHTML +=`<div class="icons"><button class="btn edit"><span><i class="bi bi-pencil-fill"></i></span><button class="btn check">${checkIcons(task)}<button class="btn remove"><span><i class="bi bi-trash3"></i></span></button></div></div>`;
+        taskDiv.innerHTML +=`<div class="icons"><button class="btn edit"><span><i class="bi bi-pencil-fill"></i></span><button class="btn remove"><span><i class="bi bi-trash3"></i></span></button></div></div>`;
         tasksContainer.append(taskDiv);
     }
 }
-
-function createTask(){
-    const inputText = document.getElementById("taskInput").value;
-    TasksManager.add(inputText);
-    renderTask();
-    clearInput();
-    enableClearAllBtn();
+const isComplete = (task) => {
+    if(task.isCompleted){
+        return "task completed";
+    }
+    if(!task.isCompleted){
+        return "task";
+    }
 }
-
-function renderTask(){
-    const tasksContainer = document.getElementById("tasksContainer");
-    let inputText = document.getElementById("taskInput").value;
-    inputText = inputText.charAt(0).toUpperCase() + inputText.slice(1);
-    let newTask = document.createElement("div");
-    newTask.className = "task";
-    const newTaskInput = document.createElement("input");
-    newTaskInput.defaultValue = `${inputText}`;
-    newTaskInput.className = "input-text";
-    newTaskInput.setAttribute("type", "text");
-    newTaskInput.setAttribute("readOnly", "enabled");
-    newTask.append(newTaskInput);
-    newTask.innerHTML += `<div class="icons"><button class="btn edit"><span><i class="bi bi-pencil-fill"></i></span><button class="btn check">${checkIcons(newTask)}<button class="btn remove"><span><i class="bi bi-trash3"></i></span></button></div></div>`;
-    newTask.dataset.id = TasksManager.getTasks().length - 1;
-    tasksContainer.append(newTask);
-    loadEventListener();
-    displayTitle();
+const displayTitle = () => {
+    const myTasksTitle = document.getElementById("myTasksTitle");
+    const tasksList = TasksManager.getTasks();
+    tasksList.length ? myTasksTitle.style.opacity = 1 : myTasksTitle.style.opacity = 0;
+    tasksList.length ? noTasks.style.display = "none" : noTasks.style.display = "block";
+    noTasks.innerHTML = `No tasks to display`;
 }
-
-function clearInput(){
+const clearInput = () => {
     const input = document.getElementById("taskInput");
     input.value = "";
     input.focus();
 }
-
-function enableClearAllBtn(){
-    tasksList.length >= 1 ? clearAllBtn.style.display = "block" : clearAllBtn.style.display = "none";
-}
-
-function clearAllTasks(){
+const removeTask = (event) => {
+    const currentTask = (event.target.closest(".task"));
+    const taskID = currentTask.getAttribute("data-id");
+    TasksManager.remove(taskID);
     const tasksList = TasksManager.getTasks();
-    for(let task = 0; task < tasksList.length; task++){
-        let taskIndex = tasksList.indexOf(task);
-        TasksManager.deleteTask(taskIndex);
-        TasksManager.tasks.pop();
-        console.log(TasksManager.getTasks());
-        const taskInDOM = document.querySelector(".task");
-        taskInDOM.remove();
-        console.log("removed");
-    }
-    localStorage.clear();
-    enableClearAllBtn();
+    const tasksContainer = document.getElementById("tasksContainer");
+    tasksContainer.innerHTML = "";
+    const noTasks = document.createElement("p");
+    noTasks.setAttribute("id", "noTasks");
+    noTasks.innerHTML = "No tasks to display";
+    tasksList.length ? "" : tasksContainer.appendChild(noTasks);
+    renderTasks(tasksList);
+    loadEventListeners();
     displayTitle();
-    window.location.reload();
+    enableClearAllBtn();
 }
-
-function loadEventListener(){
-    const checkButtons = document.querySelectorAll(".check");
-    checkButtons.forEach((button) => {
-        button.addEventListener("click", () => {
-            const currentButton = button.closest(".btn");
-            updateCompletionBtn(currentButton);
-            if(currentButton.innerHTML.includes(`<i class="bi bi-check2"></i>`)){
-                currentButton.innerHTML = `<span><i class="bi bi-arrow-counterclockwise"></i></span>`;
-                return;
-            }
-            if(currentButton.innerHTML.includes(`<i class="bi bi-arrow-counterclockwise"></i>`)){
-                currentButton.innerHTML = `<span><i class="bi bi-check2"></i></span>`;
-                return;
-            }
-        })
-    })
+const toggleStatus = (event) => {
+    const currentTask = (event.target.closest(".task"));
+    const taskInput = currentTask.querySelector(".input-text");
+    const taskObj = tasksList.filter((task) => task.task.toLowerCase() === taskInput.value.toLowerCase());
+    TasksManager.updateStatus(taskObj[0]);
+    taskObj[0].isCompleted ? currentTask.classList.add("completed") : currentTask.classList.remove("completed");
+    TasksManager.save();
+    
 }
-
-function displayTitle(){
-    const tasksList = TasksManager.getTasks();
-    tasksList.length === 0 ? myTasksTitle.style.display = "none" : myTasksTitle.style.display = "block";
-    tasksList.length === 0 ? noTasks.style.display = "block" : noTasks.style.display = "none";
-    noTasks.innerHTML = `No tasks to display`;
-}
-
-function editTask(event){
+const editTask = (event) => {
     const task = event.target.closest(".task");
     const taskInput = task.getElementsByClassName("input-text")[0];
     taskInput.removeAttribute("readOnly");
@@ -237,8 +145,7 @@ function editTask(event){
         }
     })
 }
-
-function saveEdit(event, saveButton, taskButtons){
+const saveEdit = (event, saveButton, taskButtons) => {
     const taskInput = event.target.closest(".task");
     const taskID = taskInput.getAttribute("data-id");
     const newTaskText = taskInput.getElementsByClassName("input-text")[0];
@@ -253,13 +160,41 @@ function saveEdit(event, saveButton, taskButtons){
         button.classList.remove("disabled");
     }
 }
+const clearAllTasks = () => {
+    const tasksList = TasksManager.getTasks();
+    for (let task = 0; task < tasksList.length; task++){
+        TasksManager.remove(tasksList.indexOf(tasksList[task]));
+    }
+    localStorage.clear();
+    enableClearAllBtn();
+    displayTitle();
+    window.location.reload();
+}
+const enableClearAllBtn = () => {
+    const clearAllBtn = document.getElementById("clearTasks");
+    tasksList.length ? clearAllBtn.style.display = "block" : clearAllBtn.style.display = "none";
+}
+const loadEventListeners = () => {
+    addBtn.addEventListener("click", isValid);
+    document.addEventListener("keydown", (e) => {isValid(e);})
 
-// function alreadyCreated(text){
-//     for(let task of tasksList){
-//         if(task.task === text){ 
-//             errorContainer.style.opacity = 1;
-//             errorContainer.innerText = `You already created this task.`;
-//             return;
-//         }
-//     }
-// }
+    const clearAllBtn = document.getElementById("clearTasks");
+    clearAllBtn.addEventListener("click", clearAllTasks);
+
+    const removeButtons = document.querySelectorAll(".remove");
+    removeButtons.forEach(button => {button.addEventListener("click", (event) => removeTask(event))});
+
+    const checkButtons = document.querySelectorAll(".check");
+    checkButtons.forEach(button => {button.addEventListener("click", (event) => toggleStatus(event))});
+
+    const editButtons = document.querySelectorAll(".edit");
+    editButtons.forEach(button => {button.addEventListener("click", (event) => editTask(event))});
+}
+
+
+// Init
+
+displayTitle();
+enableClearAllBtn();
+renderTasks(tasksList);
+loadEventListeners();
